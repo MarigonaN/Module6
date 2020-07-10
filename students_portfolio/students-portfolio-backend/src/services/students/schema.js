32ccce345c847c92096dffaa639557f3e482fc33
MarigonaN <marigonanuhija@gmail.com>
2020-07-10 14:55:47 +0200

    Update schema.js

--- students_portfolio/students-portfolio-backend/src/services/students/schema.js
+++ students_portfolio/students-portfolio-backend/src/services/students/schema.js
@@ -1,4 +1,5 @@
 const {Schema, model} = require('mongoose')
+const v = require("validator")
 
 const studentSchema = new Schema({
 name: {
@@ -13,9 +14,21 @@ email: {
     type: String,
     required: true,
     lowercase: true,
+    validate: async (value) => {
+        if(!v.isEmail(value)){
+            throw new Error("Email is not valid!")
+        }else{
+            const checkEmail = await studentModel.findOne({email: value})
+            if (checkEmail){
+                throw new Error("Email already in use!")
+            }
+        }
+
+    }
 },
 dateOfBirth: Date,
 Country: String,
 })
 
-module.exports = model('Student', studentSchema)
+const studentModel = model("Student", studentSchema)
+module.exports = studentModel
