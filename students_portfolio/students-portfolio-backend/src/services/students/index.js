c3bcf002b59104edad060e9c913b161f39e54f97
MarigonaN <marigonanuhija@gmail.com>
2020-07-10 13:15:07 +0200

    commit

--- students_portfolio/students-portfolio-backend/src/services/students/index.js
+++ students_portfolio/students-portfolio-backend/src/services/students/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const studentSchema = require('./schema')
+const studentModel = require('./schema')
 
 const studentsRouter = express.Router()
 
 studentsRouter.get('/', async (req, res, next) => {
     try {
-        const studentsList = await studentSchema.find(req.query)
+        const studentsList = await studentModel.find(req.query)
         res.send(studentsList)
     } catch (error) {
         next(error)
@@ -17,7 +17,7 @@ studentsRouter.get('/', async (req, res, next) => {
 
 studentsRouter.get('/:id', async (req, res, next) => {
     try {
-        const student = await studentSchema.findById(req.params.id)
+        const student = await studentModel.findById(req.params.id)
         console.log(student)
         if (student) {
             res.send(student)
@@ -37,7 +37,7 @@ studentsRouter.get('/:id', async (req, res, next) => {
 
 studentsRouter.post('/', async (req, res, next) => {
     try {
-        const newStudent = new studentSchema(req.body)
+        const newStudent = new studentModel(req.body)
         const response = await newStudent.save()
         res.send(response)
     } catch (error) {
@@ -50,7 +50,7 @@ studentsRouter.post('/', async (req, res, next) => {
 
 studentsRouter.put('/:id', async (req, res, next) => {
     try {
-        const response = await studentSchema.findByIdAndUpdate(req.params.id, req.body)
+        const response = await studentModel.findByIdAndUpdate(req.params.id, req.body)
         console.log(response)
         res.send(response)
     } catch (error) {
@@ -63,7 +63,7 @@ studentsRouter.put('/:id', async (req, res, next) => {
 
 studentsRouter.delete('/:id', async (req, res, next) => {
     try {
-        await studentSchema.findByIdAndDelete(req.params.id)
+        await studentModel.findByIdAndDelete(req.params.id)
 
         res.send("deleted!")
     } catch (error) {
