ec08ad9488661f06d1478186855c1d1fe1b10023
MarigonaN <marigonanuhija@gmail.com>
2020-07-14 16:24:28 +0200

    commit

---
+++ m6-d7-homework/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const db = require("./db")
+const studentRouter = require("./routes/students")
+
+const server = express()
+server.use(cors())
+server.use(express.json())
+
+server.get("/", (req, res)=> {
+    res.send("The server is running!")
+})
+
+server.use("/students", studentRouter)
+
+server.listen(process.env.PORT || 3456, () => console.log("Running on ", process.env.PORT || 3456))
